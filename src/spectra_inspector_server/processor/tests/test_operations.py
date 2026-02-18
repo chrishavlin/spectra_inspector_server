9910053dfee3c1bc6d725edd2c7e7364ad517852
chavlin <chris.havlin@gmail.com>
2026-02-18 14:15:02 -0600

    add test

--- src/spectra_inspector_server/processor/tests/test_operations.py
+++ src/spectra_inspector_server/processor/tests/test_operations.py
@@ -1,6 +1,7 @@
 from spectra_inspector_server._testing import _on_disc_mock
 from spectra_inspector_server.processor.operations import (
     get_sample_axes_info,
+    get_single_channel_image,
 )
 
 
@@ -8,3 +9,38 @@ def test_get_sample_axes_info(edax_path_handler):
     fake_filename = _on_disc_mock.filenames[0]
     axis_info = get_sample_axes_info(edax_path_handler, fake_filename)
     assert len(axis_info) == 3
+
+
+def test_get_single_channel_image(edax_path_handler):
+    fake_filename = _on_disc_mock.filenames[0]
+    axis_info = get_sample_axes_info(edax_path_handler, fake_filename)
+
+    im = get_single_channel_image(edax_path_handler, fake_filename, 2)
+
+    shp = [0, 0, 0]
+    for ax in axis_info:
+        shp[ax.index_in_array] = ax.size
+    expected_shp = tuple(shp[:-1])
+
+    assert im.shape == expected_shp
+
+    im = get_single_channel_image(
+        edax_path_handler, fake_filename, 2, index0_range=(0, 4)
+    )
+    new_expected = (4, expected_shp[1])
+    assert im.shape == new_expected
+
+    im = get_single_channel_image(
+        edax_path_handler, fake_filename, 2, index1_range=(0, 5)
+    )
+    new_expected = (expected_shp[1], 5)
+    assert im.shape == new_expected
+
+    im = get_single_channel_image(
+        edax_path_handler,
+        fake_filename,
+        2,
+        index0_range=(6, 8),
+        index1_range=(2, 5),
+    )
+    assert im.shape == (2, 3)
